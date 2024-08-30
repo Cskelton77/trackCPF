d4b6d39cc23bae7e380e50953c04e77fe9fb120c
Cain Skelton <cain@Cains-MacBook-Air.local>
2024-08-30 20:52:00 +0100

    Mobile layout viewport

--- src/app/tracker/layout.tsx
+++ src/app/tracker/layout.tsx
@@ -1,11 +1,10 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Login - TrackCPF',
+  title: 'Tracker - TrackCPF',
   description: 'Track your Calories, Protein, Fibre',
 };
 
