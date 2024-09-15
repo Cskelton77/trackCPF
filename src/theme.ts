2e6736f9bccc527a7887f099d86eee53f4f7484e
Cain Skelton <cain@Cains-MacBook-Air.local>
2024-09-15 23:12:44 +0100

    Plant Points get an icon

--- src/theme.ts
+++ src/theme.ts
@@ -9,6 +9,8 @@ export const theme = {
     fibreRing: '#23541B', // '#B0AB80',
     proteinRing: '#6D7393', // '#B3B3F1',
     proteinRingWarning: '#FF0000',
+
+    plantPoint: '#45f248', // '#B0AB80',
   },
   layoutPadding: '24px',
   buttonPadding: '12px',
