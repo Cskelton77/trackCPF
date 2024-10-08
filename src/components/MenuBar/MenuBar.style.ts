550b5bfb8b33e55bcfca43f60fe1c08db73ebd32
Cain Skelton <cain@cainskelton.com>
2024-10-08 21:19:37 +0100

    remove vendor prefixes

--- src/components/MenuBar/MenuBar.style.ts
+++ src/components/MenuBar/MenuBar.style.ts
@@ -2,11 +2,6 @@ import { theme } from '@/theme';
 import styled from 'styled-components';
 
 export const MenuBarContainer = styled.div`
-  position: -webkit-sticky;
-  position: -moz-sticky;
-  position: -ms-sticky;
-  position: -o-sticky;
-  position: sticky;
   height: 8vh;
   border-top: 1px solid ${theme.colours.lightGrey};
   display: flex;
